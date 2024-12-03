f1b694ce56bdd659d3786d75074df1d6c2be5769
quocanhkhuong <anh.khuong269@gmail.com>
2024-12-03 11:57:37 -0600

    Test auto-trigger when update source code

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+/*test*/
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
